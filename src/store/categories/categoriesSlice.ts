cf252e0b0900db92c88ee70f7e4f9cad18581abc
Elbaraa <baraax01@gmail.com>
2025-05-16 02:49:08 +0700

    Created store by redux toolkit

--- src/store/categories/categoriesSlice.ts
+++ src/store/categories/categoriesSlice.ts
@@ -1,11 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
+// this is the state interface for the categories slice of the Redux store.
+interface ICategoriesState {
+    records: {id: number, title: string, prefix: string, img: string}[];
+    loading: 'idle' | 'pending' | 'succeeded' | 'failed';
+    error: string | null;
+};
+
+// Define the slice
+const initialState: ICategoriesState = {
     records: [],
     loading: "idle",
     error: null,
 };
 
+// Define the action creators for the slice
 const categoriesSlice = createSlice({
     name: 'categories',
     initialState,
